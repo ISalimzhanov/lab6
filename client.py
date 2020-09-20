f10b271f3911e3e115cea92e425c85cda9e0ceae
ISalimzhanov <iskabaur@gmail.com>
2020-09-20 22:44:11 +0600

    client and server

---
+++ client.py
@@ -0,0 +1,44 @@
+import argparse
+import socket
+import os
+import threading
+
+
+def send_file(file_name: str, host: str, port: int) -> None:
+    """
+    :param file_name: name of the file.txt that should be sen
+    :param host: server's address
+    :param port: server's port
+    :return:
+    """
+    try:
+        file_size = os.path.getsize(file_name)
+        file = open(file_name, 'r')
+    except FileNotFoundError:
+        print('There is no such file.txt')
+        return
+
+    s = socket.socket()
+    s.connect((host, port))
+    s.sendall(len(file_name).to_bytes(1, byteorder='big'))  # sending file_name size
+    s.sendall(file_name.encode())  # sending file.txt name
+    sent_size = 0
+    buff_size = 1024
+    data = file.read(buff_size)
+    while data:  # sending file.txt
+        s.send(data.encode())
+        data = file.read(buff_size)
+        sent_size = min(sent_size + buff_size, file_size)
+        print(f'progress: {sent_size}/{file_size} bytes are sent')
+    file.close()
+    s.close()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file_name", help="name of the file.txt that should be sent", type=str)
+    parser.add_argument("--s_host", help="server's address", type=str)
+    parser.add_argument("--s_port", help="server's port", type=int)
+    args = parser.parse_args()
+    thread = threading.Thread(target=send_file, args=(args.file_name, args.s_host, args.s_port))
+    thread.start()
---
+++ server.py
@@ -0,0 +1,48 @@
+import socket
+import os
+import threading
+
+
+def store_file(conn, address) -> None:
+    """
+    Receiving and storing file.txt
+    :param address:
+    :param conn:
+    :return:
+    """
+    print(f'Receiving file from {address}')
+    # getting file_name
+    fname_size = int.from_bytes(conn.recv(1), byteorder='big')
+    fname = conn.recv(fname_size).decode()
+
+    # checking is there file.txt with the same file.txt name
+    cnt = 0
+    new_fname = fname  # new file.txt name
+    file_name_body, file_name_format = fname.split('.')
+    while os.path.exists(new_fname):
+        cnt += 1
+        new_fname = f'{file_name_body}_copy{cnt}.{file_name_format}'
+    # receiving data
+    file = open(new_fname, 'w')
+    buff_size = 1024
+    data = conn.recv(buff_size)
+    while data:
+        file.write(data.decode())
+        data = conn.recv(buff_size)
+    file.close()
+    print(f'Received file.txt. Stored as {new_fname}')
+
+
+def server_main():
+    s = socket.socket()
+    port = 8080
+    s.bind(('', port))
+    s.listen(10)
+    while True:
+        conn, address = s.accept()
+        thread = threading.Thread(target=store_file, args=(conn, address))
+        thread.start()
+
+
+if __name__ == '__main__':
+    server_main()
